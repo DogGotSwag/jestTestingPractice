1c5e199cc8b317fc8acd09999cc6ca862908c16c
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-04 19:31:50 -0700

    complete isDefined test & apply to every obj/func
    create test for all func/obj's to be defined
    
    makes sure they all currently exist use a describe to keep these
    separated from the rest of future test
    
    refactor is defined test to remove duplication

--- testFunctions.test.js
+++ testFunctions.test.js
@@ -1,5 +1,20 @@
-import two from "./testFunctions";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./testFunctions";
 
-it('test if function works', () => {
-    expect(two()).toBeDefined();
-})
+describe("check if all functions created", () => {
+  let list = [capitalize, reverseString, caesarCipher, analyzeArray];
+  for (let i = 0; i < list.length; i++) {
+    it(`${list[i].name} ${typeof list[i]} defined`, () => {
+      expect(list[i]).toBeDefined();
+    });
+  }
+
+  it("calculator object defined", () => {
+    expect(calculator).toBeDefined();
+  });
+});

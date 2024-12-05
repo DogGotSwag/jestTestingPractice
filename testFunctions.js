56854bb83cbb18e1261e73ac3323c0a3ee339b10
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-04 19:36:35 -0700

    create all function and obj boiler plates
    now they pass the first round of tests

--- testFunctions.js
+++ testFunctions.js
@@ -1,3 +1,14 @@
-export default () => {
-    return '';
+function capitalize() {
 }
+
+function reverseString(){
+}
+
+let calculator = {}
+
+function caesarCipher(){
+}
+
+function analyzeArray(){
+}
+export {capitalize, reverseString, calculator, caesarCipher, analyzeArray};

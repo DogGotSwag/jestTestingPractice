0777a3c011c079441acafeda8e456eeb8e002111
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-12 18:04:33 -0700

    make ceasarCipher func able to pass first 3 test
    it can now move by on or on any positive key as long as it does
    not take it out of bounds

--- testFunctions.js
+++ testFunctions.js
@@ -4,17 +4,52 @@ function capitalize(string = "") {
 }
 
 function reverseString(string = "") {
-    return string.split('').reverse().join('');
+  return string.split("").reverse().join("");
 }
 
 let calculator = {
-  add: (num, numTwo) => num + numTwo, 
-  subtract: (num, numTwo) => num - numTwo, 
+  add: (num, numTwo) => num + numTwo,
+  subtract: (num, numTwo) => num - numTwo,
   multiply: (num, numTwo) => num * numTwo,
-  divide: (num, numTwo) => num/numTwo,
+  divide: (num, numTwo) => num / numTwo,
 };
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+function caesarCipher(string, key) {
+  let currentIndex = alphabet.indexOf(string);
 
-function caesarCipher() {}
+  if (currentIndex + key > 25) {
+  } else {
+    currentIndex += key;
+  }
+  return alphabet[currentIndex];
+}
 
 function analyzeArray() {}
 export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };

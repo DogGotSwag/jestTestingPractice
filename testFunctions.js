870c7fd7d44d5b9bec134d45fdf1b8ab58434689
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-14 13:48:46 -0700

    adjust caesarCipher to have case preservation
    i added another function to check if a letter
    is upper case or not

--- testFunctions.js
+++ testFunctions.js
@@ -13,6 +13,11 @@ let calculator = {
   multiply: (num, numTwo) => num * numTwo,
   divide: (num, numTwo) => num / numTwo,
 };
+
+function isUpperCase(char) {
+  let upper = char.toUpperCase();
+  return upper.localeCompare(char) === 0 ? true : false;
+}
 let alphabet = [
   "a",
   "b",
@@ -42,22 +47,28 @@ let alphabet = [
   "z",
 ];
 function caesarCipher(string, key) {
-  let word = '';
+  let word = "";
   for (let i = 0; i < string.length; i++) {
-    let currentIndex = alphabet.indexOf(string[i]);
+    let isUpper = isUpperCase(string[i]);
+    let currentIndex = alphabet.indexOf(string[i].toLowerCase());
     let largeNewIndex = currentIndex + key;
     let finalNewIndex;
     if (largeNewIndex > 25 || largeNewIndex < 0) {
       finalNewIndex = currentIndex + (key % 26);
       if (finalNewIndex > 25) finalNewIndex -= 26;
       if (finalNewIndex < 0) finalNewIndex += 26;
-      word += alphabet[finalNewIndex];
-    } else word += alphabet[largeNewIndex];
+      word += isUpper
+        ? alphabet[finalNewIndex].toUpperCase()
+        : alphabet[finalNewIndex];
+    } else
+      word += isUpper
+        ? alphabet[largeNewIndex].toUpperCase()
+        : alphabet[largeNewIndex];
   }
   return word;
 }
 
-// console.log(caesarCipher("a", -46));
+console.log(caesarCipher("HeLLo", 3));
 
 function analyzeArray() {}
 export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
